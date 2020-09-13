4cca9df466f339126053a503ae121838155ee12d
DavidStinson <mail@davidstinson.dev>
2020-09-13 12:50:15 -0500

    first commit

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome. This is an authorization template.</h1>
+    </div>
+  );
+}
+
+export default App;
